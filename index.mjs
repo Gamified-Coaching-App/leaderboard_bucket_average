9a2a7620817cf815665a1defe154554cb3c8e590
mariashapo <mart99maria@gmail.com>
2024-02-22 10:05:22 +0000

    Process zero aggregate values so they do not skew the average

--- index.mjs
+++ index.mjs
@@ -24,7 +24,7 @@ export async function handler(event) {
 
 async function prepareAndTriggerChallengeGeneration(event) {
     console.log("prepareAndTriggerChallengeGeneration triggered");
-    const tableNameLeaderboard = "leaderboard_mock";
+    const tableNameLeaderboard = "leaderboard";
     const seasonLengthDays = 28;
 
     try {
@@ -105,14 +105,6 @@ async function makeApiCall(url, payload) {
                 'Content-Length': dataString.length,
             },
         };
-        // const options = {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         // 'Content-Length': dataString.length,
-        //     },
-        //     body: payload
-        // };
 
         const req = https.request(url, options, (res) => {
             let response = '';
@@ -205,51 +197,26 @@ async function calculateAverageSkillForBucket(tableName, bucketId, seasonLengthD
 
     const apiUrl = "https://88pqpqlu5f.execute-api.eu-west-2.amazonaws.com/dev_1/3-months-aggregate";
 
-    // const apiResponse = await makeApiCall(apiUrl, userIdsJSON);
+    // return a 3 months aggregate for each user
     const apiResponse = await fetchApiData(apiUrl, userIdsJSON);
 
     // Extract values and convert them to numbers
     const values = Object.values(apiResponse).map(value => parseInt(value, 10));
     console.log("Values: ", values);
-    // Calculate the average
-    const average = values.reduce((sum, value) => sum + value, 0) / values.length;
+
+    // sheck if all values are zeros
+    const allZeros = values.every(value => value === 0);
+
+    // if all zeros, return 0, else filter out zeros
+    const updatedValues = allZeros ? [0] : values.filter(value => value !== 0);
+    
+    // Calculate the average with updated values
+    const average = updatedValues.reduce((sum, value) => sum + value, 0) / updatedValues.length;
 
     console.log("Average:", average);
 
-    return userCount > 0 ? average / seasonLengthDays : 0;
-
-    // console.log(`Calculating average skill for bucket: ${bucketId}`);
-    // let totalSkill = 0;
-    // let userCount = 0;
-    // let params1 = {
-    //     TableName: tableName,
-    //     ProjectionExpression: "aggregate_skills_season",
-    //     FilterExpression: "bucket_id = :bucketId",
-    //     ExpressionAttributeValues: { ":bucketId": bucketId },
-    // };
-
-    // console.log(`DynamoDB Query Params: ${JSON.stringify(params)}`);
-
-    // try {
-    //     let scanResponse;
-    //     do {
-    //         scanResponse = await documentClient.scan(params).promise();
-    //         if (scanResponse.Items.length > 0) {
-    //             scanResponse.Items.forEach(item => {
-    //                 totalSkill += item.aggregate_skills_season;
-    //                 userCount += 1;
-    //             });
-    //         } else {
-    //             console.log(`No items found for bucketId ${bucketId}`);
-    //         }
-    //         params.ExclusiveStartKey = scanResponse.LastEvaluatedKey;
-    //     } while (scanResponse.LastEvaluatedKey);
-
-    //     return userCount > 0 ? (totalSkill / userCount) / seasonLengthDays : 0;
-    // } catch (error) {
-    //     console.error(`Error calculating average skill for bucket ${bucketId}:`, error);
-    //     throw error;
-    // }
+    // Dev stage: return 2000 meters per day if no data for the bucket
+    return userCount > 0 ? average / calculateDaysInMonths() : 2000;
 }
 
 
@@ -275,3 +242,19 @@ async function getUsersInBucket(tableName, bucketId) {
 
     return userIDs;
 }
+
+function calculateDaysInMonths() {
+    let totalDays = 0;
+    const now = new Date();
+  
+    for (let i = 2; i >= 0; i--) {
+      const month = new Date(now.getFullYear(), now.getMonth() - i, 1);
+      const nextMonth = new Date(now.getFullYear(), now.getMonth() - i + 1, 1);
+      const daysInMonth = (nextMonth - month) / (1000 * 60 * 60 * 24);
+      totalDays += daysInMonth;
+    }
+      
+    console.log("Total days in this month and the two months before:", totalDays);
+    return totalDays;
+  }
+  
